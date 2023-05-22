12a929888f55a65f196f3af9f5a5b9f355dc55bb
Jerzy R <jerzy.roslon@pw.edu.pl>
2023-05-22 15:07:41 +0200

    Print lista zakupow

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,6 @@ lista_zakupow = {
     'piekarnia' :  ['chleb', 'pączek', 'bułki'],
     'warzywniak' : ['marchew', 'seler', 'rukola']
 }
+
+for sklep, rzeczy in lista_zakupow.items():
+    print(f'Idę do {sklep}, kupuję tu następujące rzeczy: {rzeczy}')
