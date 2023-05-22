396b1106da797af561664aaf1b3ffc6d4d701d68
Jerzy R <jerzy.roslon@pw.edu.pl>
2023-05-22 15:10:32 +0200

    Add count feature

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,6 +2,11 @@ lista_zakupow = {
     'piekarnia' :  ['chleb', 'pączek', 'bułki'],
     'warzywniak' : ['marchew', 'seler', 'rukola']
 }
+x=0
 
 for sklep, rzeczy in lista_zakupow.items():
     print(f'Idę do {sklep.capitalize()}, kupuję tu następujące rzeczy: {list(map(str.capitalize,rzeczy))}')
+    for rzecz in rzeczy:
+        x = x + 1
+
+print(f'W sumie kupuję {x} produktów.')
